773c37cc8f161aa211c73c50fd1304a383393549
Hemanth Krishna <hkpdev008@gmail.com>
2020-08-09 20:30:23 +0530

    inital commit

---
+++ term-setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+cp .zshrc ~/.zshrc
+cp .p10k.zsh ~/.p10k.zsh
+
